f0003baa8d317a5da8dff854b7d4774379abdaac
Niels Gräber <nielsgraber@gmail.com>
2021-04-23 09:34:42 +0200

    setup of the project

---
+++ VR Glove/Assets/ReadPotentiometer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Uduino;
+
+public class ReadPotentiometer : MonoBehaviour
+{
+
+    public Slider slider;
+    UduinoManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = UduinoManager.Instance;
+
+        manager.pinMode(AnalogPin.A0, PinMode.Input);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int analogValue = manager.analogRead(AnalogPin.A0);
+        // 0  - 1024
+        slider.value = (float)analogValue / 1000.0f;
+    }
+}
